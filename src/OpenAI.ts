c849eaf13d4a9dd23f633652b932088acf53c8a7
benmills <ben@benmills.org>
2023-04-23 14:43:35 -0700

    Wip

--- src/OpenAI.ts
+++ src/OpenAI.ts
@@ -6,7 +6,7 @@ import { encode } from 'gpt-3-encoder';
 dotenv.config({ path: '.env.local' });
 
 let DEBUG = false;
-const API_URL = 'https://api.openai.com/v1/chat/completions';
+const API_URL = 'https://api.openai.com/v1';
 const API_KEY = process.env.OPENAI_KEY;
 
 if (DEBUG) {
@@ -48,17 +48,38 @@ export async function openaiChat(messages: Msg[]): Promise<string> {
     console.log("Tokens used:", getTokenCount(messages));
   }
 
-  const response = await axios.post(API_URL, requestBody, {
-    headers: {
-      'Authorization': `Bearer ${API_KEY}`,
-      'Content-Type': 'application/json',
-    }
-  });
-
+  const response = await _apiPost("/chat/completions", requestBody);
   return response.data.choices[0].message.content.trim();
 }
 
-export function getTokenCount(messages: Msg[]): number {
+export async function openaiEmbedding(input: string): Promise<number[][]> {
+  let requestBody = {
+    "model": "text-embedding-ada-002",
+    "input": input
+  };
+
+  const response = await _apiPost("/embeddings", requestBody);
+  return response.data.data[0]["embedding"];
+}
+
+async function _apiPost(urlFragment: string, requestBody: object) {
+  const url = API_URL + urlFragment;
+  try {
+    return await axios.post(url, requestBody, {
+      headers: {
+        'Authorization': `Bearer ${API_KEY}`,
+        'Content-Type': 'application/json',
+      }
+    });
+  } catch (e) {
+    console.error(e);
+    console.log("Failed making POST request to:", url);
+    console.log("Request body:", requestBody);
+    throw ("OpenAI API Error");
+  }
+}
+
+function getTokenCount(messages: Msg[]): number {
   let totalTokens = 0;
   for (const m of messages) {
     totalTokens += encode(`${m.role}: ${m.content}`).length;
--- test/kb-agent.ts
+++ test/kb-agent.ts
@@ -1,18 +1,54 @@
-import { ConvoManagerAgent } from "../src/agents";
-import { userMsg, agentMsg } from "../src/OpenAI";
+import { KnowledgeBaseAgent } from "../src/agents";
+import { userMsg, agentMsg, openaiEmbedding } from "../src/OpenAI";
+import cosineSimilarity from "cosine-similarity";
 
-async function main() {
-  const agent = new ConvoManagerAgent();
-
+async function getRealKB() {
   const exampleConvo = [
-    userMsg("I need to figure out what I need to do next week."),
-    agentMsg("Ok what are your goals for the upcoming week?"),
-    userMsg("I need to finish building a prototype for work, clean up my house, and pay my taxes")
+    userMsg("Hey nice to meet you! I want to tell you a bit about myself and who I am."),
+    agentMsg("Ok great can you tell me more about you?"),
+    userMsg("My name is Ben and I live in Redwood City, CA. I'm working on a new startup I founded with my friend Ali called Meso"),
+    agentMsg("That's awesome! Starting a new startup is exciting, but also scary. How are you coping?"),
+    userMsg("Yeah that's true I've been trying to learn how to cope with the stress better by going on walks."),
+  ];
+  const exampleTasks = [
+    { title: "Work on Meso prototype", description: "Create an example of how Meso could be used with a DEX like Uniswap. Needs to be done by the end of the week of 4/24/23.", tags: ["meso", "development", "crypto", "deadline"] }
   ];
 
-  const convoName = await agent.nameConversation(exampleConvo);
+  const agent = new KnowledgeBaseAgent();
+  await agent.extractAndSaveKeyWords(exampleConvo, exampleTasks);
 
-  console.log(convoName);
+  console.log(agent.knowledgeBase);
+}
+
+async function main() {
+  const exampleKB = [
+    {
+      title: 'User Info',
+      slug: 'user_info',
+      description: "The user's name is Ben and he lives in Redwood City, CA. He is working on a startup called Meso with his friend Ali.",
+      related: ['startup', 'location']
+    },
+    {
+      title: 'Startup Information',
+      slug: 'startup_info',
+      description: "Meso is a startup founded by Ben and Ali. Task 'Work on Meso prototype' relates to their project and involves creating an example of how Meso could be used with a DEX like Uniswap.",
+      related: ['startup', 'crypto', 'development']
+    }
+  ];
+
+  const embeddings: number[][] = await openaiEmbedding(JSON.stringify(exampleKB));
+
+  const newMessage = 'Can you tell me more about the Meso startup?';
+  const newEmbedding = await openaiEmbedding(newMessage);
+
+  const similarities = embeddings.map((embedding: object) => cosineSimilarity(embedding, newEmbedding));
+  const maxSimilarity = Math.max(...similarities);
+  const closestEmbeddings = embeddings.filter((_: object, index: number) => similarities[index] === maxSimilarity);
+
+  const relevantData = closestEmbeddings.flatMap(embedding => exampleKB.find(data => data.description.includes(embedding.join(' '))));
+
+  console.log(relevantData);
+
 }
 
 main();
